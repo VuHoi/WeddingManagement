354de32c524edcb69e231c11fed699f2eb8372e9
hoideptrai123 <toilahoi007@gmail.com>
2017-02-26 20:30:29 +0700

    Create project

---
+++ app/src/main/java/com/example/vukhachoi/weddingmanagement/HallsActivity.java
@@ -0,0 +1,13 @@
+package com.example.vukhachoi.weddingmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HallsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halls);
+    }
+}
